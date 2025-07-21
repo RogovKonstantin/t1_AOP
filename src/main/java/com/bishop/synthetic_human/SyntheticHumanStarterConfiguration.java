1e9cd6e35ec629c02050eeb4ef263e5483864249
RogovKonstantin <d50776310@gmail.com>
2025-07-21 21:51:19 +0300

    remove controller, add auto-config, test as dependency

---
+++ src/main/java/com/bishop/synthetic_human/SyntheticHumanStarterConfiguration.java
@@ -0,0 +1,10 @@
+package com.bishop.synthetic_human;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan("com.bishop.synthetic_human")
+public class SyntheticHumanStarterConfiguration {
+
+}
